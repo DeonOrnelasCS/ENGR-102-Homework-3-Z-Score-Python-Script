febb60b3caf41b822437926a4b73e3a5d37e952d
RedDeadRevolver <ornelasdeon+github@gmail.com>
2025-11-12 23:11:27 -0800

    Adding names.

--- Main.py
+++ Main.py
@@ -110,7 +110,7 @@ def z_score(x, mu, sigma):
     Returns the z-score of x
     """
 
-    # Deon Ornelas, Gena Nelson, Miles Oehm TEST
+    # Deon Ornelas, Gena Nelson, Miles Oehm TEST 2
 
     # Your code goes between this comment and the return statement
 
